ec041bf4c55d0c78c6d5a097ba5aa07fa3253508
Byron Smith <me@byronjsmith.com>
2024-09-24 11:14:10 +0100

    Add example metadata.

--- example_data/raw_reads/download_raw_reads.sh
+++ example_data/raw_reads/download_raw_reads.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-SAMPLES="HSM67VDP HSM6XRSI PSM6XBW3 PSM7J1A2 MSM79HCG MSM79HCI"
+# SAMPLES="HSM67VDP HSM6XRSI PSM6XBW3 PSM7J1A2 MSM79HCG MSM79HCI"
+SAMPLES="MSM79HCI"
 
-for sample in $SAMPLES:
+for sample in $SAMPLES
 do
     echo $sample
-    curl -O https://ibdmdb.org/downloads/raw/HMP2/MGX/2018-05-04/$sample.tar
-    tar -xf $sample.tar
+    curl -O https://ibdmdb.org/downloads/raw/HMP2/MGX/2018-05-04/$sample.tar \
+    && tar -xf $sample.tar \
+    && rm $sample.tar
 done
